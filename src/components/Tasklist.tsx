e7ed234246407387850b284d0235b10ea1e10e65
Kai Ming <gkaim9@gmail.com>
2024-11-09 22:20:03 +0800

    Update Tasklist

--- src/components/Tasklist.tsx
+++ src/components/Tasklist.tsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import "./index.css";
 
+interface Tasklist {
+  id: number;
+  text: string;
+}
+
 const Tasklist: React.FC = () => {
+  const [tasks, setTasks] = useState<Tasklist[]>([]);
+  const [newTask, setNewTask] = useState<string>("");
+  const [editTask, setEditingTask] = useState<string>("");
+  const [editedTask, setEditedTask] = useState<string>("");
+
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks((prevTasks) => [
+        ...prevTasks,
+        { id: Date.now(), text: newTask },
+      ]);
+      setNewTask("");
+    }
+  }
+
+  function handleAddTask(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === "Enter") {
+      addTask();
+    }
+  }
+
   return <div>Tasklist</div>;
 };
 
